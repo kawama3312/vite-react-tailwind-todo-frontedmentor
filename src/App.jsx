4f3bb78040900afd03659976e0f6c3b49324c817
ITConversion <109302837+ITConversion@users.noreply.github.com>
2023-10-24 13:07:34 -0600

    dia 1

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import CrossIcon from "./components/icons/CrossIcon"
+import MoonIcon from "./components/icons/MoonIcon";
+
+const App = () => {
+  return (
+    <div className="bg-[url('./assets/images/bg-mobile-light.jpg')] bg-no-repeat bg-contain min-h-screen bg-gray-200">
+        <header className="container mx-auto pt-8 px-4">
+          <div className="flex justify-between ">
+              <h1 
+                className="uppercase text-white text-3xl font-semibold tracking-widest">
+                  Todo
+              </h1>
+              <button>
+                <MoonIcon className="fill-red-400"/>
+              </button>
+          </div>
+            <form className="flex gap-4 items-center overflow-hidden rounded-md bg-white py-2 px-4 mt-8">
+              <span className="rounded-full border-2 w-5 h-5 inline-block"></span>
+              <input 
+                type="text" 
+                placeholder="Create a new todo..."
+                className="w-full text-gray-400 outline-none"
+                />
+            </form>
+         </header>
+
+         <main className="container mx-auto px-4 mt-8">
+          <div className="bg-white rounded-md [&>article]:p-4">
+              <article className="flex gap-4 border-b border-b-gray-300 ">
+                  <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
+                  <p className="text-gray-600 frow">
+                    complete online javascript curse in web</p>
+                  <button className="flex-none">
+                    <CrossIcon/>
+                  </button>
+              </article>
+              <article className="flex gap-4 border-b border-b-gray-300 ">
+                  <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
+                  <p className="text-gray-600 frow">
+                    complete online javascript curse in web</p>
+                  <button className="flex-none">
+                    <CrossIcon/>
+                  </button>
+              </article>
+              <article className="flex gap-4 border-b border-b-gray-300 ">
+                  <button className="rounded-full border-2 w-5 h-5 inline-block flex-none"></button>
+                  <p className="text-gray-600 frow">
+                    complete online javascript curse in web</p>
+                  <button className="flex-none">
+                    <CrossIcon/>
+                  </button>
+              </article>
+
+              <section className="py-4 px-4 flex justify-between">
+                <span className="text-gray-400">5 items left</span>
+                <button className="text-gray-400">clear completed</button>              
+              </section>
+          </div>
+         </main>
+
+         <section className="container mx-auto px-4 mt-8">
+            <div className="bg-white p-4 rounded-md flex justify-center gap-4">
+              <button className="text-blue-600">All</button>
+              <button className="hover:text-blue-600">Active</button>
+              <button className="hover:text-blue-600">Completed</button>
+            </div>
+         </section>
+
+         <p className="text-center mt-8">Drag and drop to reader list</p>
+    </div>
+  ) 
+}
+
+export default App;
